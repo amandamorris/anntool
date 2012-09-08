d63ed5e7c080bc0e2e52b152788c1a469db88995
unknown <amandamo@Thinkpad-2.ldc.upenn.edu>
2012-09-08 14:44:50 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anntool::Application.config.secret_token = '28783915662f42e45388462cdffb9741b3e73277dd2b22a14abaa0bc724fa5e44902ab182a20d82a347b492b6872f7d64a4da4212ac380835f1bbca96ed66144'
